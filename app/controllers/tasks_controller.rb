b48c94867b0c9c745891841aeec7e1b089b26811
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-03-03 10:50:06 +0900

    タスク一覧、詳細、新規作成画面作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def new
+    @task = Task.new
+  end
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :content)
+  end
+end
