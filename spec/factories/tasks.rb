6b63e41da3bded9e7e94ea7ae837b4770db1ca4c
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-03-11 14:21:56 +0900

    bootstrap導入完了

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,4 +6,12 @@ FactoryBot.define do
     status {'未着手'}
     priority {'高'}
   end
+  factory :second_task do
+    name { 'test_name2'}
+    content { 'test_content2'}
+    deadline {'2023-03-12'}
+    status {'着手中'}
+    priority {'中'}
+  end
+  
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,24 +1,27 @@
 require 'rails_helper'
 
-# RSpec.describe 'タスクモデル機能', type: :model do
-#   describe 'バリデーションのテスト' do
-#     context 'タスクのタイトルが空の場合' do
-#       it 'バリデーションにひっかる'do 
-#         task = Task.new(name:'',content: '失敗テスト')
-#         expect(task).not_to be_valid
-#       end
-#     end
-#     context 'タスクの詳細が空の場合' do
-#       it 'バリデーションにひっかる'  do
-#         task = Task.new(name:'失敗テスト',content:'')
-#         expect(task).not_to be_valid
-#       end
-#     end
-#     context 'タスクのタイトルと詳細に内容が記載されている場合' do
-#       it 'バリデーションが通る' do
-#         task = Task.new(name:'成功テスト',content:'成功テスト')
-#         expect(task).to be_valid
-#       end
-#     end
-#   end
-# end
+RSpec.describe 'タスクモデル機能', type: :model do
+  describe '検索機能' do
+      let!(:task) { FactoryBot.create(:task, name: 'task') }
+      let!(:second_task) { FactoryBot.create(:second_task, name: "sample") }
+      context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+        it '検索キーワードを含むタスクが絞り込まれる'do 
+          expect(Task.name_search('task')).to include(task)
+          expect(Task.name_search('task')).not_to include(second_task)
+          expect(Task.name_search('task').count).to eq 1
+        end
+      end
+      # context 'タスクの詳細が空の場合' do
+      #   it 'バリデーションにひっかる'  do
+      #     task = Task.new(name:'失敗テスト',content:'')
+      #     expect(task).not_to be_valid
+      #   end
+      # end
+      # context 'タスクのタイトルと詳細に内容が記載されている場合' do
+      #   it 'バリデーションが通る' do
+      #     task = Task.new(name:'成功テスト',content:'成功テスト')
+      #     expect(task).to be_valid
+      #   end
+      # end
+  end
+end
