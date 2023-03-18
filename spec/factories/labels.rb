4beb1e0f74999a8c76315e560165d02948779b8c
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-03-18 11:06:33 +0900

    label_spec.rbにテスト追加のため

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :test_label, class: Label do
-    label_name { "sample1" }
+    label_name { "ラベル１" }
+  end
+
+  factory :test_label2,class: Label do
+    label_name { "sample2" }
   end
 end
